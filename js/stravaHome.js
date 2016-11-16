bb4db13b1185615721f3af264a8df7938386ece3
Charles Latto <Charles Latto>
2016-11-16 22:48:43 +0000

    getting profile info

--- js/stravaHome.js
+++ js/stravaHome.js
@@ -21,20 +21,23 @@ function($scope,$location,$http){
 	}).then(function successCallback(response) {
 		//console.log(response);
 		$scope.auth_code = response.data.access_token;
+		$scope.loadProfile();
 	}, function errorCallback(response) {
 		console.log("error");
 	});
 	
-	$http({
-		method: 'GET',
-		url: "https://www.strava.com/api/v3/athlete?access_token="+$scope.auth_code
-	}).then(function successCallback(response) {
-		//console.log(response);
-		$scope.userdata = response.data;
-		console.log(response.data);
-	}, function errorCallback(response) {
-		console.log("error");
-	});
+	$scope.loadProfile = function(){
+		$http({
+			method: 'GET',
+			url: "https://www.strava.com/api/v3/athlete?access_token="+$scope.auth_code
+		}).then(function successCallback(response) {
+			//console.log(response);	
+			$scope.userdata = response.data;
+			console.log(response.data);
+		}, function errorCallback(response) {
+			console.log("error");
+		});
+	}
 	
 	
 	
