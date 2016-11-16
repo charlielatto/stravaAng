14587d0401952e59a48e9b563bfc579439b0d809
Charles Latto <Charles Latto>
2016-11-16 22:13:13 +0000

    home page with auth

---
+++ js/stravaHome.js
@@ -0,0 +1,19 @@
+var app = angular.module('stravaHome',[]);
+
+app.controller('MainCtrl',[
+'$scope',
+function($scope){
+	
+	var searchObject = $location.search();
+	
+	$http({
+		method: 'GET',
+		url: "php/form.php?code="+searchObject.code;
+	}).then(function successCallback(response) {
+		$scope.auth_code = response.access_token;
+	}, function errorCallback(response) {
+		console.log("error");
+	});
+	
+	
+}]);
